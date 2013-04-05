003b7fb668405ed3fbb8ee0e6c3429077b573113
Jos Accapadi <jos.accapadi@oregonstate.edu>
2013-04-05 02:02:12 -0700

    changes for osu

--- copy_styles.rb
+++ copy_styles.rb
@@ -2,13 +2,16 @@
 `mkdir /tmp/jenkins-isotope-style`
 `cp *.css /tmp/jenkins-isotope-style`
 `cp *.png /tmp/jenkins-isotope-style`
+`cp *.gif /tmp/jenkins-isotope-style`
 `git checkout gh-pages`
 `git fetch origin`
 `git merge origin/gh-pages`
 `cp /tmp/jenkins-isotope-style/*.css ./`
 `cp /tmp/jenkins-isotope-style/*.png ./`
+`cp /tmp/jenkins-isotope-style/*.gif ./`
 `git add *.css`
 `git add *.png`
+`git add *.gif`
 `git commit -m"Updated styles"`
 `git push origin gh-pages`
 `git checkout master`
