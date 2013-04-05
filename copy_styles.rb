0c1e9dc7ac295b6e2662148f102653e83f26faa0
Jos Accapadi <jos.accapadi@oregonstate.edu>
2013-04-05 00:55:57 -0700

    changed copy styles to add image

--- copy_styles.rb
+++ copy_styles.rb
@@ -1,11 +1,14 @@
 # Copy the styles from the master branch to the gh-pages branch
 `mkdir /tmp/jenkins-isotope-style`
 `cp *.css /tmp/jenkins-isotope-style`
+`cp *.png /tmp/jenkins-isotope-style`
 `git checkout gh-pages`
 `git fetch origin`
 `git merge origin/gh-pages`
 `cp /tmp/jenkins-isotope-style/*.css ./`
+`cp /tmp/jenkins-isotope-style/*.png ./`
 `git add *.css`
+`git add *.png`
 `git commit -m"Updated styles"`
 `git push origin gh-pages`
 `git checkout master`
